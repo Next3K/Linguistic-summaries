242424f79e302822abc35188b8c10327814ef243
Pawel <236691@edu.p.lodz.pl>
2023-05-26 17:27:36 +0200

    fixes

--- Model/src/main/java/org/example/model/db/Entry.java
+++ Model/src/main/java/org/example/model/db/Entry.java
@@ -6,15 +6,16 @@ import java.util.Map;
 
 @Getter
 public class Entry {
-    public Entry(SubjectType subjectType, Map<DatabaseColumn, Attribute> values) {
+    public Entry(SubjectType subjectType, Map<DatabaseColumn, Double> values) {
         if (subjectType == null) throw new IllegalArgumentException("Subject type cannot be null");
         if (values.size() == 0) throw new IllegalArgumentException("At least one attribute has to be chosen");
         this.subjectType = subjectType;
         this.values = values;
+        // Map.of(DatabaseColumn.AFTERNOON_HUMIDITY, 10d, DatabaseColumn.INSOLATION, 12d);
     }
 
     private SubjectType subjectType;
-    private Map<DatabaseColumn, Attribute> values;
+    private Map<DatabaseColumn, Double> values;
 
     public enum SubjectType {
         CURRENT, PREMILLENIAL
